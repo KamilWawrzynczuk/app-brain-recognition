49939192a60c6a149285b44032681e4110fb7f6e
Kamil Wawrzyńczuk <kamil.wawrzynczuk@gmail.com>
2019-02-13 23:41:11 +0100

    deploy

---
+++ precache-manifest.6b8d0b0893a2926915d2c28897a440d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8a4ad9b5a6ad37d0182d9b8b89f2c9",
+    "url": "/static/media/logo.ff8a4ad9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f39e16bf841e44f1132",
+    "url": "/static/js/main.2f39e16b.chunk.js"
+  },
+  {
+    "revision": "da42ecfa74ab6602b6bb",
+    "url": "/static/js/1.da42ecfa.chunk.js"
+  },
+  {
+    "revision": "2f39e16bf841e44f1132",
+    "url": "/static/css/main.3a8cd8d3.chunk.css"
+  },
+  {
+    "revision": "da42ecfa74ab6602b6bb",
+    "url": "/static/css/1.50cd3f30.chunk.css"
+  },
+  {
+    "revision": "e0a335f34140a7596826f847461fbd24",
+    "url": "/index.html"
+  }
+];
